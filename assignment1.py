810a214b91b7b1e8b81e887e7e8e41b833640f76
MatthewHowlett <matthew.howlett@my.jcu.edu.au>
2019-09-12 22:38:16 +1000

    Create main function

--- assignment1.py
+++ assignment1.py
@@ -1,15 +1,32 @@
 """
 Replace the contents of this module docstring with your own details
-Name:
-Date started:
-GitHub URL:
+Name:Matthew Howlett
+Date started: 2/9/2019
+GitHub URL: https://github.com/cp1404-students/2019-2-a1-MatthewHowlett
 """
 
 
 def main():
     """..."""
-    print("Movies To Watch 1.0 - by <Your Name>")
+    file = open("movies.csv", "r")
+    movies = file.readlines()
+    print("Movies To Watch 1.0 - by Matthew Howlett")
+    again = True
+    while again:
+        choice = input("Menu: \n L - List movies \n A - Add new movie \n W - Watch a movie \n Q - Quit").upper()
+        if choice == "L":
+            list_movies(movies)
+        elif choice == "A":
+            add_movie(movies)
+        elif choice == "W":
+            watch_movie(movies)
+        elif choice == "Q":
+            again = False
+            print("Have a nice day :)")
+        else:
+            print("Invalid menu choice")
+    file.close()
 
 
 if __name__ == '__main__':
-    main()
+   main()
